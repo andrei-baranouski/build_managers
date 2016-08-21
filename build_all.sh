275caad5b6bf052f42b764a05c4a7c0eb2d20574
Andrei Baranouski <andrei.baranouski@gmail.com>
2016-08-21 22:51:14 +0300

    next iter

--- build_all.sh
+++ build_all.sh
@@ -1,4 +1,5 @@
 cd ant && ant clean && ant compile && ant jar && ant run && cd .. && 
 cd maven && dir && mvn package && java -cp target/mavenexample-1.0-SNAPSHOT.jar epam.tcc.recipe.buildmanager.maven.HelloMaven && cd ..  &&
-cd gradle && gradlew build && java -cp build/libs/gradleexample-1.0-SNAPSHOT.jar epam.tcc.recipe.buildmanager.gradle.HelloGradle
+cd gradle && ls -la && 
+./gradlew build && java -cp build/libs/gradleexample-1.0-SNAPSHOT.jar epam.tcc.recipe.buildmanager.gradle.HelloGradle
 
