2e9790b7233a9127c94ad7b08392733ea0edd7a9
Andrei Baranouski <andrei.baranouski@gmail.com>
2016-08-21 22:18:00 +0300

    build_all.sh

---
+++ build_all.sh
@@ -0,0 +1,1 @@
+cd ant && ant clean && ant compile && ant jar && ant run && cd .. && cd maven && dir && mvn package && java -cp target/mavenexample-1.0-SNAPSHOT.jar epam.tcc.recipe.buildmanager.maven.HelloMaven 
